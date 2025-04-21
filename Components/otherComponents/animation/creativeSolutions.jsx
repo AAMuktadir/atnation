587aa36c0f5caf5f8025552f6945d987c7324c4e
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2025-04-21 15:43:45 +0600

    test 3

--- Components/otherComponents/animation/creativeSolutions.jsx
+++ Components/otherComponents/animation/creativeSolutions.jsx
@@ -1,15 +1,16 @@
 "use client";
+import dynamic from "next/dynamic";
 import React from "react";
-import Spline from "@splinetool/react-spline";
+
+const Spline = dynamic(() => import("@splinetool/react-spline"), {
+  ssr: false,
+});
 
 export default function CreativeSolutionAnimation() {
   return (
-    <>
-      <div className="relative w-full h-screen">
-        <Spline scene="https://prod.spline.design/J-29532UxVlnfs-Q/scene.splinecode" />
-
-        <div className="absolute inset-0 flex items-center justify-center text-white pointer-events-none"></div>
-      </div>
-    </>
+    <div className="relative w-full h-screen">
+      <Spline scene="https://prod.spline.design/J-29532UxVlnfs-Q/scene.splinecode" />
+      <div className="absolute inset-0 flex items-center justify-center text-white pointer-events-none"></div>
+    </div>
   );
 }
