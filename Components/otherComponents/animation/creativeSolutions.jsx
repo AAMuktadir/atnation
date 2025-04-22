12066e0b13484b2649454df9266b0f87576142fa
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2025-04-22 15:23:43 +0600

    test 3

--- Components/otherComponents/animation/creativeSolutions.jsx
+++ Components/otherComponents/animation/creativeSolutions.jsx
@@ -1,23 +1,15 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import dynamic from "next/dynamic";
 
-const Spline = dynamic(() => import("@splinetool/react-spline"), {
+const SplineWrapper = dynamic(() => import("./splineWrapperMarketing"), {
   ssr: false,
 });
 
 export default function CreativeSolutionAnimation() {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) return null;
-
   return (
     <div className="flex flex-col items-center justify-center">
-      <Spline scene="https://prod.spline.design/IRs-hpEoDY7olt0e/scene.splinecode" />
+      <SplineWrapper />
     </div>
   );
 }
---
+++ Components/otherComponents/animation/splineWrapperMarketing.jsx
@@ -0,0 +1,9 @@
+"use client";
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+export default function SplineWrapper() {
+  return (
+    <Spline scene="https://prod.spline.design/IRs-hpEoDY7olt0e/scene.splinecode" />
+  );
+}
