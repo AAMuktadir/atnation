8b978ce1b4fa6600917104e439e0abef6d6cbd07
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2025-04-23 00:52:54 +0600

    try 3

--- Components/testAnimation.jsx
+++ Components/testAnimation.jsx
@@ -1,9 +1,6 @@
 "use client";
-import dynamic from "next/dynamic";
-
-const Spline = dynamic(() => import("@splinetool/react-spline"), {
-  ssr: false,
-});
+import React from "react";
+import Spline from "@splinetool/react-spline";
 
 export default function TestAnimation() {
   return (
