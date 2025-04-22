f94e785631707de43c53519ca13e3868445040ef
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2025-04-22 17:32:58 +0600

    TestAnimation

--- app/marketing-solutions/page.jsx
+++ app/marketing-solutions/page.jsx
@@ -3,13 +3,13 @@ import React from "react";
 import Header from "@/Components/global/header";
 import LayoutFooter from "@/Components/global/layoutFooter";
 import SlideButton from "@/Components/digitalMarketing/slideButton";
-// import TestAnimation from "@/Components/testAnimation";
+import TestAnimation from "@/Components/testAnimation";
 
 export default function Page() {
   return (
     <div>
       <Header />
-      {/* <TestAnimation /> */}
+      <TestAnimation />
       <SlideButton />
       <LayoutFooter />
     </div>
