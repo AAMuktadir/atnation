c5f8e5ef9bb87abe6a00b80e15c1456e5fa00898
AAMuktadir <md.abdullah.al.muktadir@g.bracu.ac.bd>
2025-04-22 15:17:06 +0600

    test 2

--- Components/otherComponents/animation/creativeSolutions.jsx
+++ Components/otherComponents/animation/creativeSolutions.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 
 const Spline = dynamic(() => import("@splinetool/react-spline"), {
@@ -7,6 +7,14 @@ const Spline = dynamic(() => import("@splinetool/react-spline"), {
 });
 
 export default function CreativeSolutionAnimation() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
     <div className="flex flex-col items-center justify-center">
       <Spline scene="https://prod.spline.design/IRs-hpEoDY7olt0e/scene.splinecode" />
